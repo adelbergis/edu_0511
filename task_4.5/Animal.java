dc1de1ef1e2a012ea5aa69d9480e90630e47a3ff
adelbergis <65073505+adelbergis@users.noreply.github.com>
2021-04-20 11:52:15 +0300

    Create Animal.java

---
+++ task_4.5/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+    boolean vegetarian;
+    String speak;
+    byte paws;
+
+    public Animal(boolean vegetarian, String speak, byte paws) {
+        this.vegetarian = vegetarian;
+        this.speak = speak;
+        this.paws = paws;
+    }
+}
